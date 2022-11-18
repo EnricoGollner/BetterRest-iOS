3c70a885aef86b863ef4ad0c6f2374ec215b68e4
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2022-11-18 03:36:30 -0300

    Basic Layout

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Enrico Sousa Gollner on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
